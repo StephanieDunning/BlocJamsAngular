123d0e857fb8e20d9c5c1a0c9ea1ef84416a9a92
Stephanie Dunning <eggandcheesesandwich@gmail.com>
2016-12-01 09:03:17 -0800

    assign3: FIXED

--- dist/scripts/app.js
+++ dist/scripts/app.js
@@ -10,18 +10,19 @@
         	.state('landing', {
             	url: '/',
              	templateUrl: '/templates/landing.html'
-        });
-		
+        })
+
         	.state('album', {
             	url: '/album',
             	templateUrl: '/templates/album.html'
-        });
+        })
 		
 			.state('collection', {
             	url: '/collection',
-            	templateUrl: '/templates/collection.html'
+             	templateUrl: '/templates/collection.html'
         });
     }
+
 
 	angular
     	.module('blocJams', ['ui.router'])
